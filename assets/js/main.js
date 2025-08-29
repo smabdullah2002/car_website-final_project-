17d7e7f4fc66328d38f43f85d3e88555bd83d865
nusrat12724 <nusratzahan16126@gmail.com>
2025-08-29 20:32:45 +0600

    added navbar,home and home start button

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+// Show menu
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// Hide menu
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+//nusrat
